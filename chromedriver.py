57f439e259b5fbb8029cea76e1ad9bff8a3d74c9
jackkylin <41520756+jackkylin@users.noreply.github.com>
2019-04-16 20:46:05 +0800

    Add files via upload

---
+++ chromedriver.py
@@ -0,0 +1,9 @@
+﻿
+#This can be used as follows within a notebook
+from webdriverdownloader import ChromeDriverDownloader
+cdd = ChromeDriverDownloader()
+chromedriver, chromebin = cdd.download_and_install()
+ 
+#If required, the path to the drive can be set explicitly:
+from selenium import webdriver
+browser = webdriver.Chrome(executable_path=chromebin)
